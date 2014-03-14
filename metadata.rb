bc88cdf2df9eb7eca7f8ffea6b0758c179ee0ad5
Fraser Scott <fraser.scott@burberry.com>
2014-03-14 11:36:48 +0000

    Version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'fraser.scott@gmail.com'
 license          'MIT'
 description      'Uses chef-vault to provide an easy-to-manage Public Key Infrastructure (PKI) for servers managed by Chef.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '1.0.0'
 
 depends 'sensu_spec', '~> 0.2'
