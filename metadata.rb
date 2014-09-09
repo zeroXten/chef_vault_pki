3b40b1848244a436988a58b1fd49b18ec014e636
Fraser Scott <fraser.scott@gmail.com>
2014-09-09 16:07:22 +0100

    latest apt

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.5.4'
 
 depends 'sensu_spec', '~> 0.10'
-depends 'apt', '~> 2.3'
+depends 'apt', '~> 2.6'
